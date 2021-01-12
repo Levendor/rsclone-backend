9d07b43ba7e7e2bee35e75ec77a6a355e53d1bb7
Levendor <myth_of_lavanda@mail.ru>
2021-01-12 21:29:39 +0300

    docs: establish template

---
+++ app.ts
@@ -0,0 +1,27 @@
+import * as express from 'express';
+import appRouter from './appRouter';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', appRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.json({
+    statusCode: 404
+  });
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.json({
+    statusCode: 500,
+    message: err.message,
+    stack: err.stack
+  });
+});
+
+export default app;
